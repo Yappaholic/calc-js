28e96a5fc51157f799e5e21cd3129700d0e66a5c
Yappaholic <sav.boyar@gmail.com>
2024-04-04 11:49:35 +0300

    Make regExp grouping and real time eval

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-
+const number1 = document.querySelector("#one");
+const number2 = document.querySelector("#two");
+const number3 = document.querySelector("#three");
+const number4 = document.querySelector("#four");
+const number5 = document.querySelector("#five");
+const number6 = document.querySelector("#six");
+const number7 = document.querySelector("#seven");
+const number8 = document.querySelector("#eight");
+const number9 = document.querySelector("#nine");
+const number0 = document.querySelector("#zero");
+const buttonAdd = document.querySelector("#add");
+const buttonSubstract = document.querySelector("#substract");
+const buttonMultiply = document.querySelector("#multiply");
+const buttonDivide = document.querySelector("#divide");
+const buttonEvaluate = document.querySelector("#equal");
+const buttonClear = document.querySelector("#c");
+let numbers = [number0, number1, number2, number3, number4, number5, number6, number7, number8, number9]
+let operators = [buttonDivide, buttonMultiply, buttonAdd, buttonSubstract]
+let res = []
+for (let i = 0; i < 10; i++) {
+  numbers[i].addEventListener("click", () => res.push(i) && console.log(res))
+};
+for (let i = 0; i < 4; i++) {
+  operators[i].addEventListener("click", () =>  res.push(operators[i].textContent) && console.log(res))
+};
+buttonClear.addEventListener("click", () => res.length = 0 && console.log(res));
+buttonEvaluate.addEventListener("click", () => console.log(res.join("")))
 function operate(string) {
   let firstNumber
   let operator
@@ -28,4 +54,3 @@ function multiply(x, y) {
 function divide(x, y) {
   return x / y
 };
-console.log(operate("3 / 5"))
